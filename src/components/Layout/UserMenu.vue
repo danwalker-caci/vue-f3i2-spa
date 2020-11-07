e3641150d6ca6399994634fd3c49e43726dc27bc
Daniel Walker <daniel.walker1@caci.com>
2020-11-07 07:55:36 -0500

    Activity Bell Update 1

--- src/components/Layout/UserMenu.vue
+++ src/components/Layout/UserMenu.vue
@@ -1,12 +1,34 @@
 <template>
   <div class="user">
-    <div class="photo">
-      <img id="UserImage" :src="userpic" alt="Personal Photo" />
-    </div>
+    <b-modal id="Todos" ref="Todos" scrollable size="xl" centered hide-footer :header-bg-variant="headerBgVariant">
+      <template v-slot:modal-title>My Tasks</template>
+      <b-form>
+        <b-table id="TodosTable" :ref="TodosTable" v-model="shownData" responsive :striped="striped" :bordered="bordered" :small="small" :hover="hover" :items="mytodos" :fields="fields" :primary-key="ID" style="table-layout: fixed;">
+          <template v-slot:cell(actions)="data">
+            <b-button size="sm" class="actionbutton" @click="completeme(data.index)" title="Mark Complete">
+              <font-awesome-icon far icon="check-circle" class="icon" :style="{ color: 'green' }"></font-awesome-icon>
+            </b-button>
+            <b-button size="sm" class="actionbutton" @click="data.toggleDetails">
+              <font-awesome-icon v-if="data.detailsShowing" far class="icon" icon="minus-square" :style="{ color: 'black' }"></font-awesome-icon>
+              <font-awesome-icon v-else-if="shownData[data.index].Body !== ''" far class="icon" icon="plus-square" :style="{ color: 'green' }"></font-awesome-icon>
+              <font-awesome-icon v-else far class="icon" icon="plus-square" :style="{ color: 'black' }"></font-awesome-icon>
+            </b-button>
+          </template>
+          <template v-slot:row-details="data">
+            <b-input-group size="sm" prepend="Description">
+              <b-form-input v-model="shownData[data.index].Body"></b-form-input>
+            </b-input-group>
+          </template>
+        </b-table>
+      </b-form>
+    </b-modal>
     <div class="info">
       <a data-toggle="collapse" :aria-expanded="!isClosed" @click.stop="toggleMenu" href="#">
         <span class="UserName">
           {{ userdisplayname }}
+          <b-spinner v-if="!mytodosloaded" variant="danger" class="loading-spinner"></b-spinner>
+          <!-- <b-spinner variant="primary" class="loading-spinner"></b-spinner> -->
+          <span v-if="mytodos.length > 0" class="badge badge-xs badge-danger sidebar-badge" @click="OpenTodos">{{ mytodos.length }}</span>
           <b class="caret"></b>
         </span>
       </a>
@@ -14,12 +36,7 @@
       <div>
         <ul class="nav" v-show="!isClosed">
           <slot>
-            <li>
-              <a class="profile-dropdown" :href="userurl">
-                <span class="sidebar-mini"><font-awesome-icon fas icon="cog" class="icon"></font-awesome-icon></span>
-                <span class="sidebar-normal">My Profile</span>
-              </a>
-            </li>
+            <li></li>
           </slot>
         </ul>
       </div>
@@ -33,13 +50,7 @@ import User from '@/models/User'
 import Personnel from '@/models/Personnel'
 import Workplan from '@/models/WorkPlan'
 import Company from '@/models/Company'
-
-let SPCI = null
-if (window._spPageContextInfo) {
-  SPCI = window._spPageContextInfo
-}
-
-let baseurl = SPCI.webServerRelativeUrl
+import Todo from '@/models/Todo'
 
 export default {
   computed: {
@@ -54,6 +65,12 @@ export default {
     },
     usergroups() {
       return User.getters('CurrentUserGroups')
+    },
+    mytodosloaded() {
+      return Todo.getters('MyTodosLoaded')
+    },
+    mytodos() {
+      return Todo.getters('mytodos')
     }
   },
   props: {
@@ -66,7 +83,6 @@ export default {
     return {
       todocount: 0,
       userdisplayname: '',
-      userpic: baseurl + '/SiteAssets/html/static/img/user.png',
       userurl: '#',
       isClosed: true
     }
@@ -83,6 +99,7 @@ export default {
   mounted: function() {
     vm = this
     this.$nextTick(function() {
+      Todo.dispatch('getDigest')
       Company.dispatch('getCompanies')
       Workplan.dispatch('getDigest')
       Workplan.dispatch('getWorkplans')
@@ -90,7 +107,9 @@ export default {
       Personnel.dispatch('getPersonnel')
       if (!vm.userloaded) {
         User.dispatch('getUserId').then(function() {
-          vm.$options.interval = setInterval(vm.getUserProfile, 1000)
+          User.dispatch('getUserProfile').then(function() {
+            vm.$options.interval = setInterval(vm.updateUserInfo, 1000)
+          })
         })
       }
     })
@@ -99,31 +118,39 @@ export default {
     toggleMenu() {
       this.isClosed = !this.isClosed
     },
-    getUserProfile: function() {
+    updateUserInfo() {
       clearInterval(this.$options.interval)
-      User.dispatch('getUserProfile').then(function() {
-        vm.$options.interval = setInterval(vm.updateUserPic, 1000)
-      })
-    },
-    updateUserPic: function() {
-      clearInterval(this.$options.interval)
-      try {
-        this.userpic = this.profiledata.PictureUrl
-        this.userdisplayname = this.profiledata.DisplayName
-        this.userurl = this.profiledata.PersonalUrl
-      } catch (e) {
-        // do not care
-      }
+      this.userdisplayname = this.profiledata.DisplayName
       User.dispatch('getUserGroups').then(function() {
-        vm.$options.interval = setInterval(vm.waitForGroups, 1000)
+        Todo.dispatch('getTodosByUser', vm.UserId).then(function() {
+          console.log('USERMENU MOUNT COMPLETED')
+        })
       })
     },
-    waitForGroups: function() {
-      if (this.usergroups && this.usergroups.length > 0) {
-        clearInterval(this.$options.interval)
-      }
+    OpenTodos: function() {
+      this.$bvModal.show('Todos')
+    },
+    refreshMyTodos: function() {
+      Todo.dispatch('getTodosByUser', this.UserId)
+    },
+    completeme: function(idx) {
+      let payload = {}
+      payload.id = this.shownData[idx].id
+      payload.uri = this.shownData[idx].uri
+      payload.etag = this.shownData[idx].etag
+      Todo.dispatch('completeTodo', payload).then(function() {
+        let userid = vm.UserId
+        Todo.dispatch('getTodosByUser', userid)
+      })
     }
   },
+  cron: [
+    {
+      time: 10000,
+      method: 'refreshMyTodos',
+      autoStart: true
+    }
+  ],
   beforeDestroy() {
     clearInterval(this.$options.interval)
   }
