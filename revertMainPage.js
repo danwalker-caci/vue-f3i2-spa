2d21414c9d0a329317b5e258a34e736031bd6cbd
Drew Ahrens <drew.ahrens@caci.com>
2020-11-23 04:26:05 -0500

    Added security view for approval/rejection - 11/23/2020 4:26 AM

--- revertMainPage.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('#app').style.display = 'none'
-document.querySelector('#suiteBar').style.display = 'block'
-document.querySelector('#s4-ribbonrow').style.display = 'block'
-document.querySelector('#sideNavBox').style.display = 'block'
---
+++ src/components/Security/securityview.vue
@@ -0,0 +1,14 @@
+<template>
+  <!-- Add Form where users can download master docs and upload the changed docs. -->
+  <b-container fluid class="contentHeight p-0" id="MainContainer">
+    <!-- Use the upload travel docs as a template for uploading documents -->
+    <!-- If user is SUBC, only load the related personnel to their company. 
+         Otherwise, load companies into dropdown and on user selection load all personnel related to that company into a dropdown. -->
+    <!-- on submission, split the personnel first name, last name out of the dropdown. -->
+    <div id="form" class="col-12 p-4"></div>
+  </b-container>
+</template>
+<script>
+export default {}
+</script>
+<style lang="sass"></style>
