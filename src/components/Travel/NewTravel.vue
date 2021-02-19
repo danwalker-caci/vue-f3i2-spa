371751d3702b367ef2cc97f06e9a6d9e78e2fb3a
Daniel Walker <daniel.walker1@caci.com>
2021-02-19 08:34:28 -0500

    Update NewTravel.vue

--- src/components/Travel/NewTravel.vue
+++ src/components/Travel/NewTravel.vue
@@ -597,7 +597,7 @@ export default {
         ]
       },
       NewTravelfilterSettings: { type: 'Menu' },
-      fieldsFirstTab: ['WorkPlan', 'Company', 'start', 'end', 'TravelFrom', 'TravelTo', 'IndexNumber'],
+      fieldsFirstTab: ['WorkPlan', 'Company', 'start', 'end', 'TravelFrom', 'TravelTo'],
       fieldsThirdTab: ['Sponsor', 'EstimatedCost', 'Comments', 'Clearance'],
       fieldsFourthTab: ['Clearance', 'POCName', 'POCEmail', 'POCPhone'],
       travelerData: [],
