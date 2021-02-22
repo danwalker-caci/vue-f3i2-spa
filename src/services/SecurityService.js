6d54d19fa82e2f7d58bccd6b04e4e39a151ad797
Drew Ahrens <drew.ahrens@caci.com>
2021-02-22 16:43:57 -0500

    Reverting to original list items - 2/22/2021 4:43 PM

--- src/services/SecurityService.js
+++ src/services/SecurityService.js
@@ -10,8 +10,8 @@ let absurl = SPCI.webAbsoluteUrl
 let relurl = SPCI.webServerRelativeUrl
 let formurlstart = SPCI.webServerRelativeUrl + "/_api/web/lists/getbytitle('"
 let formurlend = "')/RootFolder/Files/Add"
-//let securityformurl = SPCI.webServerRelativeUrl + "/_api/Web/Lists/getbytitle('SecurityForms')/items"
-let securityformurl = SPCI.webServerRelativeUrl + "/_api/Web/Lists/getbytitle('TestSecurityForms')/items"
+let securityformurl = SPCI.webServerRelativeUrl + "/_api/Web/Lists/getbytitle('SecurityForms')/items"
+//let securityformurl = SPCI.webServerRelativeUrl + "/_api/Web/Lists/getbytitle('TestSecurityForms')/items"
 
 export default {
   getFormDigest() {
@@ -118,8 +118,8 @@ export default {
       headers: headers
     }
     let itemprops = {
-      //__metadata: { type: 'SP.Data.SecurityFormsListItem' },
-      __metadata: { type: 'SP.Data.TestSecurityFormsListItem' },
+      __metadata: { type: 'SP.Data.SecurityFormsListItem' },
+      //__metadata: { type: 'SP.Data.TestSecurityFormsListItem' },
       Title: payload.Title,
       PersonnelID: payload.PersonnelID,
       PersonName: payload.PersonName,
