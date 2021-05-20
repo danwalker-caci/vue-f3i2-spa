d9917873703583f2daa756dbbcb718e5017db2bf
drewahrens-caci <drew.ahrens@caci.com>
2021-05-20 15:09:22 -0400

    Working checks between Security and Personnel in Admin page - 5/20/2021 3:09 PM

--- src/components/Admin/admin.vue
+++ src/components/Admin/admin.vue
@@ -9,6 +9,14 @@
       </template>
       <b-progress :variant="busyVariant" :value="busyValue" :max="busyMax" show-progress animated></b-progress>
     </b-toast>
+    <b-modal title="Persons Missing in Security" id="PerSecResults" ok-only ok-variant="secondary" ok-title="Close">
+      <p>These individuals are not aligned between the Security and Personnel Lists. Please manually review to ensure dataa integrity.</p>
+      <b-table-lite id="PersonnelVSecurity" :items="persVSecurityResults" :fields="persVSecurityFields" striped></b-table-lite>
+      <p>Here are the duplicates in the Personnel List.</p>
+      <b-table-lite id="duplicatePersons" :items="duplicatePersonResults" :fields="duplicatePersonFields" striped></b-table-lite>
+      <p>Here are the duplicates in the Security List.</p>
+      <b-table-lite id="duplicatePersons" :items="duplicateSecurityResults" :fields="duplicateSecurityFields" striped></b-table-lite>
+    </b-modal>
     <b-row class="mb-1">
       <b-col cols="3">
         <b-card border-variant="success" text-variant="dark">
@@ -81,6 +89,21 @@
           </b-card-body>
         </b-card>
       </b-col>
+      <b-col cols="3">
+        <b-card border-variant="success" text-variant="dark">
+          <template v-slot:header>
+            <h3 class="mb-0">
+              <span class="ml-0">Personnel Actions</span>
+              <font-awesome-icon fas icon="people-arrow" class="icon text-danger float-right ml-1"></font-awesome-icon>
+            </h3>
+          </template>
+          <b-card-body>
+            <b-row class="p-0 m-0">
+              <b-button ref="checkPersonnelSecurity" :disabled="disablePersonnelSecurityCheck" variant="success" @click="checkPersonnelSecurity">Check Personnel Versus Security</b-button>
+            </b-row>
+          </b-card-body>
+        </b-card>
+      </b-col>
     </b-row>
   </b-container>
 </template>
@@ -147,6 +170,9 @@ export default {
     allworkplans() {
       return Workplan.getters('allWorkplans')
     },
+    allsecurity() {
+      return Security.getters('SecurityForms')
+    },
     travel() {
       return Travel.getters('allTravel')
     },
@@ -192,6 +218,14 @@ export default {
       selecteddata: null,
       ManagerEmail: '',
       cloneMSR: false,
+      disablePersonnelSecurityCheck: false,
+      showOkPersSecurityAlert: false,
+      duplicatePersonResults: [],
+      duplicatePersonFields: [{ key: 'FirstName' }, { key: 'LastName' }, { key: 'Company' }],
+      duplicateSecurityResults: [],
+      duplicateSecurityFields: [{ key: 'FirstName' }, { key: 'LastName' }, { key: 'Company' }],
+      persVSecurityResults: [],
+      persVSecurityFields: [{ key: 'id' }, { key: 'FirstName' }, { key: 'LastName' }, { key: 'Company' }],
       dataMap: [],
       tripcount: 0
     }
@@ -253,6 +287,50 @@ export default {
           console.log('Install Failed: ' + args.get_message())
         }
       )
+    },
+    async checkPersonnelSecurity() {
+      this.disablePersonnelSecurityCheck = true
+      this.persVSecurityResults = []
+      await Personnel.dispatch('getDigest')
+      await Security.dispatch('getDigest')
+      await Personnel.dispatch('getPersonnel')
+      await Security.dispatch('getSecurityForms')
+      if (this.allpersonnel.length !== this.allsecurity.length) {
+        // Loop through and find based on PersonnelID
+        this.allpersonnel.forEach(person => {
+          var result = vm.allsecurity.find(security => {
+            return security.PersonnelId === person.Id
+          })
+          var duplicateSecurity = []
+          if (result === undefined) {
+            vm.persVSecurityResults.push({ id: person.id, FirstName: person.FirstName, LastName: person.LastName, Company: person.Company })
+          } else {
+            duplicateSecurity.push({ id: person.id, FirstName: person.FirstName, LastName: person.LastName, Company: person.Company })
+          }
+          var duplicatePersons = []
+          var duplicatePerson = vm.allpersonnel.find(person2 => {
+            return person.LastName === person2.LastName && person.Company === person2.Company
+          })
+          if (duplicatePerson !== undefined) {
+            duplicatePersons.push({ id: person.id, FirstName: person.FirstName, LastName: person.LastName, Company: person.Company })
+          }
+          if (duplicatePersons.length > 1) {
+            vm.duplicatePersonResults.push({ FirstName: person.FirstName, LastName: person.LastName, Company: person.Company })
+          }
+          if (duplicateSecurity.length > 1) {
+            vm.duplicateSecurityResults.push({ FirstName: person.FirstName, LastName: person.LastName, Company: person.Company })
+          }
+        })
+        console.log('Results from checking Personnel against Security: ' + JSON.stringify(this.persVSecurityResults))
+        if (this.persVSecurityResults.length > 0) {
+          this.persVSecurityResults.reverse()
+          this.$bvModal.show('PerSecResults')
+        }
+      } else {
+        // This is fine.
+        this.showOkPersSecurityAlert = true
+      }
+      this.disablePersonnelSecurityCheck = false
     },
     async btn_LateTripReports_Clicked() {
       // calculate late trip reports
