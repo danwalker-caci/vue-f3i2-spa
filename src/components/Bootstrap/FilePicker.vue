179aa7134230da20d2e1dac714435f947a8b4bf4
Daniel Walker <daniel.walker1@caci.com>
2021-06-24 15:25:37 -0400

    Update FilePicker.vue

--- src/components/Bootstrap/FilePicker.vue
+++ src/components/Bootstrap/FilePicker.vue
@@ -3,7 +3,7 @@
     <b-row no-gutters class="m-0">
       <b-col cols="12" class="p-0">
         <b-modal id="FilePickerModal" centered header-bg-variant="light-blue" size="xl" header-text-variant="light">
-          <template v-slot:modal-title>Selected Files</template>
+          <template v-slot:modal-title>Selected Files Check</template>
           <b-container class="p-0">
             <b-row>
               <b-col cols="12">
@@ -15,6 +15,9 @@
                     <b-button class="actionbutton text-white" variant="success" @click="fixFile(shownData[row.index].id)" v-b-tooltip.hover.v-dark title="Fix File Name Issues">
                       <font-awesome-icon far icon="edit" class="icon"></font-awesome-icon>
                     </b-button>
+                    <b-button v-if="fileExists(shownData[row.index].id)" class="actionbutton text-white m-1" variant="warning" @click="overwriteFile(shownData[row.index].id)" v-b-tooltip.hover.v-dark title="Delete Existing File">
+                      <font-awesome-icon far icon="trash-alt" class="icon"></font-awesome-icon>
+                    </b-button>
                     <b-button v-if="fileExists(shownData[row.index].id)" class="actionbutton text-white m-1" variant="danger" @click="deleteFile(shownData[row.index].id)" v-b-tooltip.hover.v-dark title="Delete Existing File">
                       <font-awesome-icon far icon="trash-alt" class="icon"></font-awesome-icon>
                     </b-button>
@@ -22,14 +25,38 @@
                   <template #cell(name)="row">
                     <b-form-input class="form-control" size="sm" v-model="shownData[row.index].name" :id="getID('txt-', shownData[row.index].id)" @input="onInput(shownData[row.index].id)"></b-form-input>
                   </template>
-                  <template #cell(issues)="row">
+                  <template #cell(isSpecial)="row">
+                    <b-button v-if="shownData[row.index].isSpecial" class="actionbutton text-white m-1" variant="danger">
+                      <font-awesome-icon far icon="times-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                    <b-button v-else class="actionbutton text-white m-1" variant="success">
+                      <font-awesome-icon far icon="check-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                  </template>
+                  <template #cell(isLong)="row">
+                    <b-button v-if="shownData[row.index].isLong" class="actionbutton text-white m-1" variant="danger">
+                      <font-awesome-icon far icon="times-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                    <b-button v-else class="actionbutton text-white m-1" variant="success">
+                      <font-awesome-icon far icon="check-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                  </template>
+                  <template #cell(isExisting)="row">
+                    <b-button v-if="shownData[row.index].isExisting" class="actionbutton text-white m-1" variant="danger">
+                      <font-awesome-icon far icon="times-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                    <b-button v-else class="actionbutton text-white m-1" variant="success">
+                      <font-awesome-icon far icon="check-circle" class="icon"></font-awesome-icon>
+                    </b-button>
+                  </template>
+                  <!-- <template #cell(issues)="row">
                     <div v-for="issue in shownData[row.index].issues" :key="issue">
                       <p v-if="issue == 'isOk'">No issues in the selected file.</p>
                       <p v-if="issue == 'isLong'">The selected file is too long.</p>
                       <p v-if="issue == 'isSpecial'">The selected file contains special characters.</p>
                       <p v-if="issue == 'isExisting'">The selected file already exists.</p>
                     </div>
-                  </template>
+                  </template> -->
                 </b-table>
               </b-col>
             </b-row>
@@ -50,6 +77,14 @@ props: (passed from parent component)
   library: string - represents the name of the document library the files will be uploaded to.
   checkExists: boolean - determines if selected files should be checked for existence in the document library.
   config: string[JSON] - contains the configuration for file naming conventions and other rules.
+    {
+      allowedFiles: ['.pdf', '.docx'] (or pass empty array [] for no restrictions)
+      addTimestamp: true/false (unix based on date/time)
+      addPrefix: 'Prefix'
+      addSuffix: 'Suffix'
+      preLength: 100 (size to initially test length of filenames)
+      postLength: 250 (tested last to be sure rules don't create file name that is too large.)
+    }
 
 items: array - Array of selected files. Manually created with a globally unique id (guid) and fileBuffer
 
@@ -76,8 +111,11 @@ export default {
       shownData: [],
       fields: [
         { key: 'actions', label: 'Actions' },
-        { key: 'name', label: 'Name' },
-        { key: 'issues', label: 'Issues', thClass: 'px300' }
+        { key: 'name', label: 'Name', thClass: 'px300' },
+        { key: 'isSpecial', label: 'File has special characters.' },
+        { key: 'isLong', label: 'File is too long.' },
+        { key: 'isExisting', label: 'File already exists.' }
+        /* { key: 'issues', label: 'Issues', thClass: 'px300' } */
       ],
       hasIssues: false,
       totalRows: 1,
@@ -102,28 +140,33 @@ export default {
         let item = {}
         // build item array
         let name = String(SelectedFile.name)
-        let issues = []
+        // let issues = []
         let isLong = name.length > 100 ? true : false
         if (isLong) {
-          issues.push('isLong')
+          // issues.push('isLong')
+          item.isLong = true
           vm.hasIssues = true
         }
         let isSpecial = regex.test(name) == true ? false : true
         if (isSpecial) {
-          issues.push('isSpecial')
+          // issues.push('isSpecial')
+          item.isSpecial = true
           vm.hasIssues = true
         }
-        if (!isLong && !isSpecial) {
-          issues.push('isOk')
-        }
-        let isExisting = await this.fileExistsInLibrary(name, this.library)
-        if (isExisting) {
-          issues.push('isExisting')
+        // Only check existing files if they are not too long or if they don't have special characters as those files would not exist in the library
+        // TODO: add naming convention rules first before checking if the file exists. Need to build the name from the rules and then check
+        if (!isSpecial && !isLong) {
+          let isExisting = await this.fileExistsInLibrary(name, this.library)
+          if (isExisting) {
+            // issues.push('isExisting')
+            item.isExisting = true
+          }
         }
         item.id = Math.uuid()
         item.name = name
-        item.issues = issues
+        // item.issues = issues
         item.buffer = await this.getFileBuffer(SelectedFile)
+        item.overwrite = false
         this.items.push(item)
       })
       vm.totalRows = vm.items.length
@@ -170,6 +213,14 @@ export default {
       for (let i = 0; i < this.items.length; i++) {
         if (this.items[i].id === id) {
           this.items.splice(i, 1)
+        }
+      }
+    },
+    overwriteFile(id) {
+      // loop through files for the id and then remove the file from the array
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i].id === id) {
+          this.items[i].overwrite = true
         }
       }
     },
