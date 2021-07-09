c1c6e94f8ef31410cd4aac6ade79688e78fa476c
Daniel Walker <daniel.walker1@caci.com>
2021-07-09 06:58:32 -0400

    New Test Site

--- src/components/Layout/TopNavbar.vue
+++ src/components/Layout/TopNavbar.vue
@@ -48,7 +48,7 @@
             <template slot="button-content">
               <font-awesome-icon fas icon="cog" class="cog"></font-awesome-icon>
             </template>
-            <b-dropdown-item
+            <!-- <b-dropdown-item
               v-if="isDeveloper"
               href="#"
               onclick="javascript:if (LaunchCreateHandler('PublishingPage')) { SP.SOD.executeFunc('sp.ui.dialog.js', 'SP.UI.ModalDialog.showModalDialog', function() { var dlgOptions = { url:'\u002fsites\u002ff3i2\u002f_layouts\u002f15\u002fCreatePublishingPageDialog.aspx', autoSize: true, autoSizeStartWidth: 550 };  SP.UI.ModalDialog.showModalDialog(dlgOptions); }); };"
@@ -57,17 +57,13 @@
                 <font-awesome-icon far icon="file-alt" class="icon"></font-awesome-icon>
                 <span>Add a page</span>
               </div>
-            </b-dropdown-item>
-            <!-- <b-dropdown-item
-              v-if="isDeveloper"
-              href="#"
-              onclick="javascript:SP.SOD.executeFunc('sp.ui.pub.ribbon.js', 'Pub.Ribbon.PubCommands', function() {Pub.Ribbon.PubCommands.notifyProgress(SP.Publishing.Resources.notificationMessageLoading);if (document.forms['aspnetForm']['MSOLayout_InDesignMode'] != null) document.forms['aspnetForm']['MSOLayout_InDesignMode'].value = 1;if (document.forms['aspnetForm']['MSOAuthoringConsole_FormContext'] != null) document.forms['aspnetForm']['MSOAuthoringConsole_FormContext'].value = 1;if (document.forms['aspnetForm']['MSOSPWebPartManager_DisplayModeName'] != null) document.forms['aspnetForm']['MSOSPWebPartManager_DisplayModeName'].value = 'Design';__doPostBack('ctl05','edit')});"
-            >
-              <div class="row">
-                <font-awesome-icon far icon="edit" class="icon"></font-awesome-icon>
-                <span>Edit page</span>
-              </div>
             </b-dropdown-item> -->
+            <b-dropdown-item v-if="isDeveloper" href="#" @click="gitRdone('CreatePublishingPageDialog.aspx')">
+              <div class="row">
+                <font-awesome-icon far icon="file-alt" class="icon"></font-awesome-icon>
+                <span>Add a page</span>
+              </div>
+            </b-dropdown-item>
             <b-dropdown-item
               v-if="isDeveloper"
               href="#"
@@ -78,25 +74,25 @@
                 <span>Edit page</span>
               </div>
             </b-dropdown-item>
-            <b-dropdown-item v-if="isDeveloper" href="#" onclick="GoToPage('\u002fsites\u002ff3i2\u002f_layouts\u002f15\u002faddanapp.aspx');">
+            <b-dropdown-item v-if="isDeveloper" href="#" @click="gitRdone('addanapp.aspx')">
               <div class="row">
                 <font-awesome-icon far icon="plus-square" class="icon"></font-awesome-icon>
                 <span>Add an app</span>
               </div>
             </b-dropdown-item>
-            <b-dropdown-item v-if="isDeveloper" href="#" onclick="STSNavigate2(event,'/sites/f3i2/_layouts/15/DesignSite.aspx');">
+            <b-dropdown-item v-if="isDeveloper" href="#" @click="gitRdone('DesignSite.aspx')">
               <div class="row">
                 <font-awesome-icon fas icon="pencil-alt" class="icon"></font-awesome-icon>
                 <span>Design Manager</span>
               </div>
             </b-dropdown-item>
-            <b-dropdown-item v-if="isDeveloper" href="#" onclick="STSNavigate2(event,'/sites/f3i2/_layouts/15/viewlsts.aspx');">
+            <b-dropdown-item v-if="isDeveloper" href="#" @click="gitRdone('viewlsts.aspx')">
               <div class="row">
                 <font-awesome-icon fas icon="box-open" class="icon"></font-awesome-icon>
                 <span>Site contents</span>
               </div>
             </b-dropdown-item>
-            <b-dropdown-item v-if="isDeveloper" href="#" onclick="GoToPage('\u002fsites\u002ff3i2\u002f_layouts\u002f15\u002fsettings.aspx');">
+            <b-dropdown-item v-if="isDeveloper" href="#" @click="gitRdone('settings.aspx')">
               <div class="row">
                 <font-awesome-icon fas icon="cogs" class="icon"></font-awesome-icon>
                 <span>Site settings</span>
@@ -108,7 +104,7 @@
                 <span>Show Activity Log</span>
               </div>
             </b-dropdown-item>
-            <b-dropdown-item href="#" onclick="STSNavigate2(event,'/sites/f3i2/_layouts/15/SignOut.aspx');">
+            <b-dropdown-item href="#" @click="gitRdone('SignOut.aspx')">
               <div class="row">
                 <font-awesome-icon fas icon="sign-out-alt" class="icon"></font-awesome-icon>
                 <span>Logout</span>
@@ -126,6 +122,15 @@ import Personnel from '@/models/Personnel'
 import { isNullOrUndefined } from 'util'
 
 let vm = null
+
+/* var slash = '/'
+var tp1 = String(window.location.protocol)
+var tp2 = String(window.location.host) */
+
+let SPCI = null
+if (window._spPageContextInfo) {
+  SPCI = window._spPageContextInfo
+}
 
 export default {
   computed: {
@@ -167,6 +172,10 @@ export default {
         clearInterval(this.$options.interval)
         vm.formatContacts(this.contacts)
       }
+    },
+    gitRdone: function(page) {
+      let url = SPCI.webServerRelativeUrl + '/_layouts/15/' + page
+      window.location = url
     },
     formatContacts: function(contacts) {
       let c = []
